4b697f93940d73dd432e54be6ade7a60f585933a
oosuhada <185910926+oosuhada@users.noreply.github.com>
2026-05-05 20:52:15 +0900

    chore: configure web workspace

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: 'class',
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
